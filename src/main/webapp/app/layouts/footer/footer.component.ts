2c7accb6a7a17a7541eb7edb529a04171f138e80
Chris Kettenbach <christopher.kettenbach@primitivelogic.com>
2022-02-25 14:01:59 -0800

    initial commit

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-console */
+import { Component, AfterContentInit, ContentChild, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent implements AfterContentInit, AfterViewInit {
+  @ViewChild('intheview') viewcontent!: ElementRef;
+
+  @ContentChild('findmenow') projectedcontent!: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log('This content', this.projectedcontent);
+    this.projectedcontent.nativeElement.setAttribute('style', 'color:purple;');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('This viewcontent', this.viewcontent);
+    this.viewcontent.nativeElement.setAttribute('style', 'color:green;');
+  }
+}
